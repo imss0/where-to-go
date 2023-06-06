03246841b76a2ba87a3d23d41cdddf0b559f06e9
sohyun <shlim0410@gmail.com>
2023-06-06 10:08:12 +0100

    initialize git

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import PlaceList from "./PlaceList";
+export default function App() {
+  return (
+    <main>
+      <h2>Countries I wanna go to🛫</h2>
+      <PlaceList />
+      <h2>Countries I have been to 👣</h2>
+      <h2>Countries I like💕</h2>
+    </main>
+  );
+}
